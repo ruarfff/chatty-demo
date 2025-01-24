96d0c0344f3491fd5dd046c51a076b8a18efcf37
Ruairí O'Brien <1150322+ruarfff@users.noreply.github.com>
2025-01-24 12:24:57 -0600

    Add extension code

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,37 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "chatty" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('chatty.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from chatty!');
-	});
+	vscode.chat.createChatParticipant(
+		"chatty",
+		async (request, context, response, token) => {
+			let query = request.prompt;
+			query = `${query} - Always end every message with some form of passive aggressive 'You're welcome from chatty.`;
 
-	context.subscriptions.push(disposable);
+			const chatModels = await vscode.lm.selectChatModels({ family: "gpt-4" });
+			const messages = [vscode.LanguageModelChatMessage.User(query)];
+			const chatRequest = await chatModels[0].sendRequest(
+				messages,
+				undefined,
+				token,
+			);
+
+			for await (const token of chatRequest.text) {
+				response.markdown(token);
+			}
+		},
+	);
+
+	vscode.commands.registerCommand("chatty.summarizeSomething", () => {
+		vscode.commands.executeCommand("wokspace.action.chat.open", "@chatty Summarize this");
+	});
 }
 
 // This method is called when your extension is deactivated
